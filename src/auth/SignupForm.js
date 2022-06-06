e974c8e9a1f4105ce7d4573f06ab3b9a9d4c6710
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2022-06-05 23:45:56 -0400

    small update

--- src/auth/SignupForm.js
+++ src/auth/SignupForm.js
@@ -51,7 +51,7 @@ function SignupForm({ signup }) {
   
   
   return (
-    <section className="d-flex justify-content-center" style={{ margin: '50px'}}>
+    <section className="col-md-6 col-lg-4 offset-md-3 offset-lg-4">
     <Card >
       <CardBody>
         <CardTitle className="font-weight-bold text-center text-info">
